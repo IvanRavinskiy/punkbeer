5bc0f12668296de44129a38dfa5ca70fc73dff9e
iwann <ivan.ravinskiy@gmail.com>
2022-03-29 23:05:02 +0300

    add logic in createAPI module, use module in web

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,11 @@
 import { greeting } from "@iwann/greeting";
+import { createAPI } from "@iwann/api";
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+    console.log(createAPI().getBeerRandom.then(res => res.data))
     return (
         <div className="App">
             <header className="App-header">
--- components/api/src/index.ts
+++ components/api/src/index.ts
@@ -1,3 +1,120 @@
-export function createAPI(url: string) {
-    return `API, ${url}!`;
+import axios from "axios";
+
+export const createAPI = () => {
+
+    const instance = axios.create({
+        baseURL: 'https://api.punkapi.com/v2/'
+    })
+
+    const getBeer = instance.get<BeerType[]>('beers')
+    const getBeerRandom = instance.get<BeerType[]>('beers/random')
+
+    return {getBeer, getBeerRandom};
+}
+
+type NullableType<T> = T | null
+
+type BeerType = {
+    id: number,
+    name: string,
+    tagline: string,
+    first_brewed: string,
+    description: string,
+    image_url: string,
+    abv: NullableType<number>,
+    ibu: NullableType<number>,
+    target_fg: NullableType<number>,
+    target_og: NullableType<number>,
+    ebc: NullableType<number>,
+    srm: NullableType<number>,
+    ph: NullableType<number>,
+    attenuation_level: NullableType<number>,
+    volume: {
+        value: NullableType<number>,
+        unit:  NullableType<string>,
+    },
+    boil_volume: {
+        value: NullableType<number>,
+        unit: NullableType<string>,
+    },
+    method: {
+        mash_temp: [
+            {
+                temp: {
+                    value: NullableType<number>,
+                    unit: NullableType<string>,
+                },
+                duration: NullableType<number>
+            }
+        ],
+        fermentation: {
+            temp: {
+                value: NullableType<number>,
+                unit: NullableType<string>,
+            }
+        },
+        twist: NullableType<string>,
+    },
+    ingredients: {
+        malt: [
+            {
+                name: NullableType<string>,
+                amount: {
+                    value: NullableType<number>,
+                    unit: NullableType<string>,
+                }
+            }
+        ],
+        hops: [
+            {
+                name: NullableType<string>,
+                amount: {
+                    value: NullableType<number>,
+                    unit: NullableType<string>,
+                },
+                add: NullableType<string>,
+                attribute: NullableType<string>,
+            },
+            {
+                name: NullableType<string>,
+                amount: {
+                    value: NullableType<number>,
+                    unit: NullableType<string>,
+                },
+                add: NullableType<string>,
+                attribute: NullableType<string>,
+            },
+            {
+                name: NullableType<string>,
+                amount: {
+                    value: NullableType<number>,
+                    unit: NullableType<string>,
+                },
+                add: NullableType<string>,
+                attribute: NullableType<string>,
+            },
+            {
+                name: NullableType<string>,
+                amount: {
+                    value: NullableType<number>,
+                    unit: NullableType<string>,
+                },
+                add: NullableType<string>,
+                attribute: NullableType<string>,
+            },
+            {
+                name: NullableType<string>,
+                amount: {
+                    value: NullableType<number>,
+                    unit: NullableType<string>,
+                },
+                add: NullableType<string>,
+                attribute: NullableType<string>,
+            }
+        ],
+        yeast: NullableType<string>,
+    },
+    food_pairing: NullableType<string[]>,
+    brewers_tips: NullableType<string>,
+    contributed_by: string,
 }
