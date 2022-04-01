6f98d2baeb75eb8b8370cb9684cfbd8478a0fbd1
iwann <ivan.ravinskiy@gmail.com>
2022-04-01 12:10:08 +0300

    add env in web via craco config

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,11 @@
 import { greeting } from "@iwann/greeting";
-import env from "@iwann/envconfig"
-// import { createAPI } from "@iwann/api";
+import { createAPI } from "@iwann/api";
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    // console.log(createAPI('https://api.punkapi.com/v2/').getBeerRandom.then(res => res.data))
-    // console.log(createAPI(`${process.env.REACT_APP_BASE_URL}`).getBeerRandom.then(res => res.data))
-    console.log(env)
+    console.log(createAPI(`${process.env.REACT_APP_BASE_URL}`).getBeerRandom.then(res => res.data))
     return (
         <div className="App">
             <header className="App-header">
--- components/envconfig/src/index.ts
+++
@@ -1,8 +0,0 @@
-import path from "path";
-// const path = require('path');
-
-require('dotenv').config({
-    path: path.join(process.cwd(), '..', '..', '.env'),
-});
-
-module.exports = process.env;
