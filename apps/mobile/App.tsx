135590fda6bd446ab3fb255376c5cd2b154d40ec
iwann <ivan.ravinskiy@gmail.com>
2022-04-01 17:20:55 +0300

    add env in mobile

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {greeting} from '@iwann/greeting';
+import Config from "react-native-config";
 
 const App = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>LALALALALA</Text>
+      <Text>{`${Config.REACT_APP_BASE_URL}`}</Text>
       <Text>{greeting()}</Text>
     </View>
   );
