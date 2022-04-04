6ea963ad48fdb2f7f6cc0b3559da64979744b9c3
iwann <ivan.ravinskiy@gmail.com>
2022-04-04 16:23:46 +0300

    env

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,36 +1,16 @@
-import {greeting} from "@iwann/greeting";
-import {createAPI} from "@iwann/api";
-import {decreaseAC, increaseAC} from "@iwann/store/src/appReducer";
-import { AppRootStateType } from "@iwann/store";
+import { greeting } from "@iwann/greeting";
+import { createAPI } from "@iwann/api";
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {useDispatch, useSelector} from "react-redux";
-
 
 function App() {
     console.log(createAPI(`${process.env.REACT_APP_BASE_URL}`).getBeerRandom.then(res => res.data))
-
-    const value = useSelector((state: AppRootStateType) => state.app.value)
-    const dispatch = useDispatch()
-
-    const onIncreaseClick = () => {
-        dispatch(increaseAC(value))
-    }
-    const onDecreaseClick = () => {
-        dispatch(decreaseAC(value))
-    }
-
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                {greeting()}
-                <div>
-                    value: {value}
-                    <button onClick={onIncreaseClick}>+</button>
-                    <button onClick={onDecreaseClick}>-</button>
-                </div>
+                <img src={ logo } className="App-logo" alt="logo"/>
+                { greeting() }
             </header>
         </div>
     );
