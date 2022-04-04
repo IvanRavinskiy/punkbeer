95e9ef09f55dbd65c87fa19f1dd2759ad2f4374b
iwann <ivan.ravinskiy@gmail.com>
2022-04-04 16:12:02 +0300

    add redux in web

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,16 +1,36 @@
-import { greeting } from "@iwann/greeting";
-import { createAPI } from "@iwann/api";
+import {greeting} from "@iwann/greeting";
+import {createAPI} from "@iwann/api";
+import {decreaseAC, increaseAC} from "@iwann/store/src/appReducer";
+import { AppRootStateType } from "@iwann/store";
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import {useDispatch, useSelector} from "react-redux";
+
 
 function App() {
     console.log(createAPI(`${process.env.REACT_APP_BASE_URL}`).getBeerRandom.then(res => res.data))
+
+    const value = useSelector((state: AppRootStateType) => state.app.value)
+    const dispatch = useDispatch()
+
+    const onIncreaseClick = () => {
+        dispatch(increaseAC(value))
+    }
+    const onDecreaseClick = () => {
+        dispatch(decreaseAC(value))
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src={ logo } className="App-logo" alt="logo"/>
-                { greeting() }
+                <img src={logo} className="App-logo" alt="logo"/>
+                {greeting()}
+                <div>
+                    value: {value}
+                    <button onClick={onIncreaseClick}>+</button>
+                    <button onClick={onDecreaseClick}>-</button>
+                </div>
             </header>
         </div>
     );
