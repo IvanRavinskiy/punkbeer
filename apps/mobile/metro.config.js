5f20de96da6f2b1eec67f69be8a020081dc098f6
Dmitrij Podabed <iam@itfarrier.com>
2022-04-07 08:26:39 +0300

    change metro.config.js using monorepo-tools

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,50 +1,26 @@
-// const path = require('path');
-//
-// module.exports = {
-//     transformer: {
-//         getTransformOptions: async () => ({
-//             transform: {
-//                 experimentalImportSupport: false,
-//                 inlineRequires: false,
-//             },
-//         }),
-//     },
-//     resolver: {
-//         extraNodeModules: {
-//             react: path.resolve(__dirname, 'node_modules/react'),
-//             'react-native': path.resolve(__dirname, 'node_modules/react-native'),
-//             'react-redux': path.resolve(__dirname, 'node_modules/react-reduxe'),
-//             'redux': path.resolve(__dirname, 'node_modules/redux'),
-//             '@reduxjs/toolkit': path.resolve(__dirname, '../../components/store'),
-//             '@babel/runtime': path.resolve(__dirname, 'node_modules/@babel/runtime'),
-//         },
-//         watchFolders: [
-//             // path.resolve(__dirname, '../../components'),
-//             path.resolve(__dirname, '../../node_modules'),
-//         ],
-//     }
-// }
+const {
+  getMetroAndroidAssetsResolutionFix,
+  getMetroTools,
+} = require('react-native-monorepo-tools');
 
-const path = require('path');
+const {extraNodeModules, watchFolders} = getMetroTools();
 
-const currentDir = __dirname;
+const androidAssetsResolutionFix = getMetroAndroidAssetsResolutionFix();
 
-const config = {
-    watchFolders: [
-        path.resolve(currentDir, '../../components'),
-        path.resolve(currentDir, '../../node_modules'),
-    ],
-
-    resolver: {
-        extraNodeModules: {
-            react: path.resolve(__dirname, '../../node_modules/react'),
-            'react-native': path.resolve(currentDir, 'node_modules/react-native'),
-            'react-redux': path.resolve(currentDir, '../../node_modules/react-redux'),
-            'redux': path.resolve(currentDir, '../../node_modules/redux'),
-            // '@reduxjs/toolkit': path.resolve(currentDir, '../../components/store'),
-            '@babel/runtime': path.resolve(currentDir, 'node_modules/@babel/runtime'),
-        },
+module.exports = {
+  resolver: {extraNodeModules},
+  server: {
+    enhanceMiddleware: middleware => {
+      return androidAssetsResolutionFix.applyMiddleware(middleware);
     },
+  },
+  transformer: {
+    getTransformOptions: async () => {
+      return {
+        transform: {experimentalImportSupport: false, inlineRequires: true},
+      };
+    },
+    publicPath: androidAssetsResolutionFix.publicPath,
+  },
+  watchFolders,
 };
-
-module.exports = config;
