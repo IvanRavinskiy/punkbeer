e72d65824353047635f0a0101496c487cc720abe
iwann <ivan.ravinskiy@gmail.com>
2022-04-22 12:35:09 +0300

    refactor web app

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,31 +1,27 @@
 import { greeting } from "@iwann/greeting";
 import { AppRootStateType } from "@iwann/store";
-import { decrease, getBeerFetch, increase } from "@iwann/store/src/appReducer";
+import { getBeerFetch } from "@iwann/store/src/appReducer";
 import { useAppDispatch, useAppSelector } from "@iwann/store/src/hooks";
 import React from "react";
 import "./App.css";
 
 function App() {
-  const value = useAppSelector((state: AppRootStateType) => state.app.value);
-  const beers = useAppSelector((state: AppRootStateType) => state.app.beers);
+  const beerRandom = useAppSelector(
+    (state: AppRootStateType) => state.app.beerRandom
+  );
   const dispatch = useAppDispatch();
 
   const onGetBeerHandler = () => {
     dispatch(getBeerFetch());
   };
-  const onIncreaseClick = () => {
-    dispatch(increase(value));
-  };
-  const onDecreaseClick = () => {
-    dispatch(decrease(value));
-  };
+
   return (
     <div className="App">
       <header className="App-header">
         {greeting()}
         <div>
           <h2>RANDOM BEER</h2>
-          {beers.map((beer) => {
+          {beerRandom.map((beer) => {
             return (
               <div key={beer.id}>
                 <div>Beer title: {beer.name}</div>
@@ -38,11 +34,6 @@ function App() {
             );
           })}
           <button onClick={onGetBeerHandler}>get random beer</button>
-        </div>
-        <div>
-          value: {value}
-          <button onClick={onIncreaseClick}>+</button>
-          <button onClick={onDecreaseClick}>-</button>
         </div>
       </header>
     </div>
