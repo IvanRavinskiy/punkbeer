271250081ee4c79c8b216eeba271d0a517ce2a87
iwann <ivan.ravinskiy@gmail.com>
2022-04-28 22:50:45 +0300

    refactor cities screen

--- apps/mobile/src/screens/CitiesScreen.tsx
+++ apps/mobile/src/screens/CitiesScreen.tsx
@@ -4,42 +4,43 @@ import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParamList} from '../navigation/Navigator';
 
+const coordinates = [
+  {
+    city: 'Minsk',
+    lat: 53.893009,
+    lgt: 27.567444,
+  },
+  {
+    city: 'Soligorsk',
+    lat: 52.789947,
+    lgt: 27.535962,
+  },
+  {
+    city: 'Kiev',
+    lat: 50.450001,
+    lgt: 30.523333,
+  },
+];
+
 export const CitiesScreen = () => {
   type Props = StackNavigationProp<RootStackParamList>; // what is 2 param????
   const navigation = useNavigation<Props>();
+
+  const foo = ({city, lat, lgt}: any) => {
+    navigation.navigate('MapContainer', {city, lat, lgt});
+  };
+
   return (
     <View>
       <Text>Select city</Text>
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate('MapContainer', {
-            city: 'Minsk',
-            lat: 53.893009,
-            lgt: 27.567444,
-          })
-        }>
-        <Text>Minsk</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate('MapContainer', {
-            city: 'Soligorsk',
-            lat: 52.789947,
-            lgt: 27.535962,
-          })
-        }>
-        <Text>Soligorsk</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate('MapContainer', {
-            city: 'Kiev',
-            lat: 50.450001,
-            lgt: 30.523333,
-          })
-        }>
-        <Text>Kiev</Text>
-      </TouchableOpacity>
+      {coordinates.map(({city, lat, lgt}, i) => {
+        const qwe = () => foo({city, lat, lgt});
+        return (
+          <TouchableOpacity key={i} onPress={qwe}>
+            <Text>{city}</Text>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 };
