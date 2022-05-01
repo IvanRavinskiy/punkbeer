41ed5773d264127774924aedda962e1f0c83a80c
iwann <ivan.ravinskiy@gmail.com>
2022-05-01 12:43:42 +0300

    refactor map screen

---
+++ apps/mobile/src/assets/magicNumber/mapsRegionInitData.ts
@@ -0,0 +1,9 @@
+export const mapsRegionInitData = {
+  latDelta: 0.0922,
+  lgtDelta: 0.0421,
+};
+
+export const mapsMiamiRegion = {
+  lat: 25.761681,
+  lgt: -80.191788,
+};
---
+++ apps/mobile/src/screens/MapScreen/types.ts
@@ -0,0 +1,4 @@
+export type MapPropsType = {
+  lat: number;
+  lgt: number;
+};
