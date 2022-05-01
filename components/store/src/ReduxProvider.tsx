ffa152426cf51aa11590c185cd956bd31b115535
iwann <ivan.ravinskiy@gmail.com>
2022-05-01 21:19:58 +0300

    fix type in redux provider

--- components/store/src/ReduxProvider.tsx
+++ components/store/src/ReduxProvider.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import { ComponentType } from 'react';
 
 import { Provider } from 'react-redux';
 import { Store } from 'redux';
 
-export const ReduxProvider = (App: any, store: Store) => {
+export const ReduxProvider = (App: ComponentType, store: Store) => {
   return () => (
     <Provider store={store}>
       <App />
