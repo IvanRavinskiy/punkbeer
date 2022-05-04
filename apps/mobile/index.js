952868f6afffaa6ce29da19a7e8dee5943db1750
iwann <ivan.ravinskiy@gmail.com>
2022-05-04 15:05:34 +0300

    refactor mobile app by redux provider

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,10 +1,7 @@
-import { createGlobalStore, ReduxProvider } from "@iwann/store";
 import { AppRegistry } from "react-native";
 
-import { App } from "./src/App";
+import { AppComp } from "./src/App";
 import { name as appName } from "./app.json";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React from "react";
 
-const { store } = createGlobalStore(AsyncStorage);
-
-AppRegistry.registerComponent(appName, () => ReduxProvider(App, store));
+AppRegistry.registerComponent(appName, () => AppComp);
--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { Navigator } from "./navigation/StackNavigator";
+import { createGlobalStore, ReduxProvider } from "@iwann/store";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const App = () => {
   return (
@@ -8,4 +10,9 @@ export const App = () => {
       <Navigator />
     </NavigationContainer>
   );
+};
+
+const { store } = createGlobalStore(AsyncStorage);
+export const AppComp = () => {
+  return <ReduxProvider App={App} store={store} />;
 };
